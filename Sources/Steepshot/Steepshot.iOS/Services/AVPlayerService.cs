f50702794aceb4e766a0fa41024fa9e7c78a4fca
Maxim Matusevich <imulir@gmail.com>
2018-04-13 15:34:53 +0300

    AVPlayer service

---
+++ Sources/Steepshot/Steepshot.iOS/Services/AVPlayerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AVFoundation;
+using Steepshot.iOS.Cells;
+
+namespace Steepshot.iOS.Services
+{
+    public class AVPlayerService
+    {
+        private static AVPlayerService thisRef;
+
+        public AVPlayer AVPlayer;
+        public NewFeedCollectionViewCell container;
+
+        public static AVPlayerService Instance
+        { 
+            get
+            {
+                if (thisRef == null)
+                    thisRef = new AVPlayerService();
+
+                return thisRef;
+            }
+        }
+
+        public AVPlayerService() { }
+    }
+}
